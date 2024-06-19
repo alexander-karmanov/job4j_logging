c277b9271d1133b08b145a3cd78c3fa6542513e9
alexander-karmanov <lektor_md@mail.ru>
2024-06-19 21:44:19 +0300

    Logging

--- src/main/java/ru/job4j/logging2/Logger.java
+++ src/main/java/ru/job4j/logging2/Logger.java
@@ -2,12 +2,16 @@ package ru.job4j.logging2;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class Logger {
     private String name;
     private LogLevel level;
     private Appender appender;
+
+    private List<Appender> appenders;
 
     public Logger(String name, LogLevel level) {
         this.name = name;
@@ -25,13 +29,25 @@ public class Logger {
         return properties;
     }
 
+    public boolean getAppender() {
+        String[] array = getConfig().getProperty("appenders").split(",");
+        List<String> list = Arrays.asList(array);
+
+        for (String st : list) {
+            appenders.add(appender);
+        }
+        return true;
+    }
+
     public boolean log(LogLevel level, String message) {
-        if (getConfig().getProperty("appender").contains("ConsoleAppender")) {
+
+        /*if (getConfig().getProperty("appenders").contains("ConsoleAppender")) {
              appender = new ConsoleAppender();
         }
-        if (getConfig().getProperty("appender").contains("FileAppender")) {
+        if (getConfig().getProperty("appenders").contains("FileAppender")) {
              appender = new FileAppender("logs.txt");
-        }
+        } */
+
         appender.append(message);
         return true;
     }
@@ -54,10 +70,5 @@ public class Logger {
     public boolean error(String message) {
         log(LogLevel.ERROR, message);
         return true;
-    }
-
-    public static void main(String[] args) {
-        Logger logger = new Logger("TestLogger", LogLevel.DEBUG);
-        logger.log(LogLevel.DEBUG, "This is a debug message");
     }
 }
